4f3087b46bc42419bbae87461633a1e1e8a1d620
Diube00 <124744187+Tiume001@users.noreply.github.com>
2026-01-21 01:20:08 +0100

    Integrate AdSense script into analytics-config.js
    Added AdSense script loading to analytics-config.js.

--- analytics-config.js
+++ analytics-config.js
@@ -1,3 +1,11 @@
+// Codice AdSense
+const adSenseScript = document.createElement('script');
+adSenseScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5649862133349961";
+adSenseScript.async = true;
+adSenseScript.crossOrigin = "anonymous";
+document.head.appendChild(adSenseScript);
+
+// ... qui sotto lasci il resto del codice di Analytics che avevi già ...
 // Sostituisci G-XXXXXXXXXX con il tuo ID di misurazione di Google Analyticstt
 const GA_ID = 'G-ZCHVRHTMYD'; 
 
